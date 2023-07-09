3bc591a981e4fb0f6d54eff0d9b1620882896985
unknown <arsal0344@gmail.comm>
2023-07-09 17:00:18 +0500

    initial and user api added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const bodyParser = require('body-parser');
 const express= require("express");
 const { PrismaClient } = require('@prisma/client');
-
 const app = express();
 const prisma = new PrismaClient();
 
@@ -11,21 +10,12 @@ app.use(bodyParser.json());
 app.get("/",(req,res)=>{
 res.send("app is working")
 });
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
-app.post('/users', async (req, res) => {
-  const { name, email } = req.body;
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-    },
-  });
-  res.json(user);
-});
-// Load the /users routes
-//app.use("/users", require("./routes/users.js"));
 
-app.listen(5000);
+//users
+const userRoute = require('./routes/users')
+app.use('/api/user',userRoute)
+
+
+
+const port = 5002
+app.listen(port,console.log(`Listening on port ${port}...`));
