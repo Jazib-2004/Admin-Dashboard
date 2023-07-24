09e54e761cfa1015e0cc342c9120245d5a210bba
Muhammad Haris Azaz Khan <77955039+haris-khan1596@users.noreply.github.com>
2023-07-24 15:28:19 +0500

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { validateToken } = require('../middlewares/auth');
-const {auth, register, login} = require('../controllers/users')
+const {auth, register, login, getUsers } = require('../controllers/users')
 
 
 router.get('/auth',validateToken,auth)
 router.post('/register',register)
 router.post('/login',login)
-
+router.get('/',getUsers)
 module.exports = router
