08448ef276c8bf35cc9685c68f1e39684d0d1adb
Jazib <jazibmemon12@gmail.com>
2023-07-24 05:11:02 +0500

    Added Sidebar, Navbar, 2 Routes, Dummy User Table in the User tab

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { useStateContext } from "./contexts/ContextProvider";
+import AppRoutes from "./AppRoutes/AppRoutes";
+const App = () => {
+  const { activeMenu } = useStateContext();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      {/* Main App */}
+      <div className="flex relative dark:bg-black-dark-bg">
+        {/* Sidebar */}
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar bg-white dark:bg-secondary-dark-bg">
+            <Sidebar />
+          </div>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg">
+            <Sidebar />
+          </div>
+        )}
+        {/* Navbar*/}
+        <div
+          className={`bg-main-bg dark:bg-main-dark-bg min-h-screen w-full ${
+            activeMenu ? "md:ml-72" : "flex-2"
+          }`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="fixed bg-main-bg dark:bg-main-dark-bg w-full navbar md:static">
+            <Navbar />
+          </div>
+          {/* App Routes */}
+          <AppRoutes/>
+          
+        </div>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
